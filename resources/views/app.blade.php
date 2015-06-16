945157d7a0a94fbf01e9a1a679a3f6b95b0a987c
hutrap <hutrap@yahoo.co.uk>
2015-06-16 18:30:45 +0100

    Add Comments  page with Angular too top navigation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,6 +26,16 @@
     <link rel="stylesheet"
           href="{{asset('assets/site/css/lightbox.min.css')}}"/>
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> <!-- load fontawesome -->
+    <style>
+        body        { padding-top:30px; }
+        form        { padding-bottom:20px; }
+        .comment    { padding-bottom:20px; }
+    </style>
+
+
     @yield('styles')
 
     <!-- Fonts -->
@@ -37,6 +47,16 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- JS -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script> <!-- load angular -->
+
+    <!-- ANGULAR -->
+    <!-- all angular resources will be loaded from the /public folder -->
+    <script src="../js/controllers/mainCtrl.js"></script> <!-- load our controller -->
+    <script src="../js/services/commentService.js"></script> <!-- load our service -->
+    <script src="../js/app.js"></script> <!-- load our application -->
 
     <link rel="shortcut icon" href="{{{ asset('assets/site/ico/favicon.ico') }}}">
 </head>
--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,30 +1,9 @@
-<!doctype html> <html lang="en"> <head> <meta charset="UTF-8"> <title>Laravel and Angular Comment System</title>
+@extends('app')
+@section('title') Comments :: @parent @stop
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> <!-- load fontawesome -->
-    <style>
-        body        { padding-top:30px; }
-        form        { padding-bottom:20px; }
-        .comment    { padding-bottom:20px; }
-    </style>
+@section('content')
 
-    <!-- JS -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script> <!-- load angular -->
-
-    <!-- ANGULAR -->
-    <!-- all angular resources will be loaded from the /public folder -->
-    <script src="../js/controllers/mainCtrl.js"></script> <!-- load our controller -->
-    <script src="../js/services/commentService.js"></script> <!-- load our service -->
-    <script src="../js/app.js"></script> <!-- load our application -->
-
-
-</head>
-<!-- declare our angular app and controller -->
-<body class="container" ng-app="commentApp" ng-controller="mainController">
-
-<div class="col-md-8 col-md-offset-2">
+    <div class="container col-md-8 col-md-offset-2" ng-app="commentApp" ng-controller="mainController">
 
     <!-- PAGE TITLE =============================================== -->
     <div class="page-header">
@@ -64,5 +43,4 @@
     </div>
 
 </div>
-</body>
-</html>
+@endsection
